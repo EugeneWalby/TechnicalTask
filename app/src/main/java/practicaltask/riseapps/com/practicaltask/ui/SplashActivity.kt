2189690af84de9f8abfea25a924cd1a035217c2d
Eugene Volboyenko <eugene.volboyenko@chisw.com>
2018-07-15 16:14:46 +0300

    Added splash screen

---
+++ app/src/main/java/practicaltask/riseapps/com/practicaltask/ui/SplashActivity.kt
@@ -0,0 +1,33 @@
+package practicaltask.riseapps.com.practicaltask.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_splash.*
+import practicaltask.riseapps.com.practicaltask.R
+import practicaltask.riseapps.com.practicaltask.ui.base.BaseActivity
+
+class SplashActivity : BaseActivity() {
+    companion object {
+        private const val DELAY_BEFORE_MOVE = 2000L
+    }
+
+    override fun provideLayout(): Int {
+        return R.layout.activity_splash
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        waitUntilNextScreen()
+    }
+
+    private fun waitUntilNextScreen() {
+        pbLoading.visibility = View.VISIBLE
+        Handler().postDelayed(
+                {
+                    moveToScreen(MainActivity::class.java)
+                    pbLoading.visibility = View.GONE
+                },
+                DELAY_BEFORE_MOVE)
+    }
+}
--- app/src/main/java/practicaltask/riseapps/com/practicaltask/ui/base/BaseActivity.java
+++ app/src/main/java/practicaltask/riseapps/com/practicaltask/ui/base/BaseActivity.java
@@ -1,8 +1,28 @@
 package practicaltask.riseapps.com.practicaltask.ui.base;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import practicaltask.riseapps.com.practicaltask.ui.SplashActivity;
+
 @SuppressLint("Registered")
-public class BaseActivity extends AppCompatActivity implements BaseView {
+public abstract class BaseActivity extends AppCompatActivity implements BaseView {
+    protected abstract int provideLayout();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(provideLayout());
+    }
+
+    protected void moveToScreen(@NonNull final Class<?> screenToMove) {
+        final Intent intent = new Intent(this, screenToMove);
+        startActivity(intent);
+        finish();
+    }
 }
